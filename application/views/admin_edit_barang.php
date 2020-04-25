c9209dac50fb9116ca6ed34f53a25d562e4ba92b
Muhammad Sulthan Angka Kurniawan <45189140+sulthankurniawan@users.noreply.github.com>
2020-04-25 20:48:16 +0700

    view

--- application/views/admin_edit_barang.php
+++ application/views/admin_edit_barang.php
@@ -1,9 +1,11 @@
-<!DOCTYPE html>
+<!doctype html>
+<html class="no-js" lang="en"></html>
+
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Medistock.go - Login</title>
+        <title>Medistock.go - Registrasi</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -11,25 +13,39 @@
 
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
     </head>
-	<body class='bg-primary'>
+
+    <body class="bg-primary">
+
+        <?php include('admin_nav.php'); ?>
 
         <center>
             <div class="col-lg-6">
                 <div class="card" style="margin-top:100px">
-                    <div class="card-header">Login</div>
+                    <div class="card-header">Registrasi</div>
                     <div class="card-body card-block">
                         <form action="" method="post" class="">
                             <div class="form-group">
                                 <div class="input-group">
-                                    <input type="text" id="username" name="username" class="form-control" placeholder="Username">
+                                    <input type="text" id="nama_barang" name="nama_barang" class="form-control" placeholder="nama barang">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <div class="input-group">
-                                    <input type="password" id="password" name="password" class="form-control" placeholder="Password">
+                                    <input type="text" id="kategori" name="kategori" class="form-control" placeholder="kategori">
                                 </div>
-                            <div class="form-actions form-group" style="margin-top:10px">
-                                <button type="submit" class="btn btn-primary btn-sm">Login</button>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input type="text" id="gambar" name="gambar" class="form-control" placeholder="gambar">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input type="email" id="deskripsi" name="deskripsi" class="form-control" placeholder="deskripsi">
+                                </div>
+                            </div>
+                            <div class="form-actions form-group">
+                                <button type="submit" class="btn btn-primary btn-sm">Submit</button>
                             </div>
                         </form>
                     </div>
